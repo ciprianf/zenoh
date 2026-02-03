e9bc8cea19f359fc0ec65810b1187dfe50181b29
Alexandru Farcasanu <alexandru@flexion.ai>
2026-02-03 15:33:35 +0100

    Fix ";"

--- zenoh/src/net/routing/dispatcher/pubsub.rs
+++ zenoh/src/net/routing/dispatcher/pubsub.rs
@@ -363,7 +363,7 @@ pub fn route_data(
                             } else {
                                 inc_stats!(outface, tx, admin, msg.payload)
                             }
-                            tracing::info!("Sending push for key_expr: {}, reliability: {:?}", key_expr, reliability);
+                            tracing::info!("Sending push for key_expr: {}, reliability: {:?}", key_expr, reliability)
                             outface.primitives.send_push(
                                 &mut Push {
                                     wire_expr: key_expr,
@@ -374,7 +374,7 @@ pub fn route_data(
                                 },
                                 reliability,
                             )
-                            tracing::info!("Push sent for key_expr: {}, reliability: {:?}", key_expr, reliability);
+                            tracing::info!("Push sent for key_expr: {}, reliability: {:?}", key_expr, reliability)
                         }
                     }
                 }
